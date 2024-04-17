87c2ab6c15069468018a74ef7ccc9ca062e00a33
OlenaDmytriieva <144238849+OlenaDmytriieva@users.noreply.github.com>
2024-04-17 22:10:02 +0300

    image loading was moved to the app component

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,48 @@ import { SearchBar } from "./components/SearchBar/SearchBar";
 import { Container } from "./components/Container/Container";
 import { Section } from "./components/Section/Section";
 import { ImageGallery } from "./components/ImageGallery/ImageGallery";
+import { getPhotos } from "./apiServise/photos";
+import { ErrorMessage } from "./components/ErrorMessage/ErrorMessage";
+import { Loader } from "./components/Loader/Loader";
+import { LoadMoreBtn } from "./components/LoadMoreBtn/LoadMoreBtn";
 
-import { useState } from "react";
+import { useState, useEffect, useRef } from "react";
 
 export default function App() {
   const [query, setQuery] = useState("");
+  const [page, setPage] = useState(1);
+  const [photos, setPhotos] = useState([]);
+  const curQuery = useRef("");
+  const [loading, setloading] = useState(false);
+  const [isError, setIsError] = useState(false);
+
+  useEffect(() => {
+    if (!query) return;
+    if (curQuery.current != query) setPhotos([]);
+    curQuery.current = query;
+
+    setloading(true);
+    const fetchPhotos = async () => {
+      try {
+        const photos = await getPhotos(query, page);
+        console.log(photos.results);
+        setPhotos((prevPhotos) => [...prevPhotos, ...photos.results]);
+      } catch (error) {
+        setIsError(true);
+        console.log(error.message);
+      } finally {
+        setloading(false);
+      }
+    };
+    fetchPhotos();
+  }, [page, query]);
 
   const onFormSubmit = (query) => {
     setQuery(query);
+  };
+
+  const onLoadMore = () => {
+    setPage(page + 1);
   };
 
   return (
@@ -17,7 +51,13 @@ export default function App() {
       <Container>
         <div>
           <SearchBar onSubmit={onFormSubmit} />
-          <ImageGallery query={query} />
+
+          {isError && <ErrorMessage />}
+          <ImageGallery photos={photos} />
+          {loading && <Loader />}
+          {photos.length > 0 && (
+            <LoadMoreBtn onClick={onLoadMore} setPage={setPage} />
+          )}
         </div>
       </Container>
     </Section>
--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -1,44 +1,10 @@
-import { getPhotos } from "../../apiServise/photos";
-import { useState, useEffect, useRef } from "react";
+import { useState } from "react";
 import { ImageCard } from "../ImageCard/ImageCard";
-import { LoadMoreBtn } from "../LoadMoreBtn/LoadMoreBtn";
 import { ImageModal } from "../ImageModal/ImageModal";
-import { ErrorMessage } from "../ErrorMessage/ErrorMessage";
-import { Loader } from "../Loader/Loader";
 import style from "./ImageGallery.module.css";
 
-export const ImageGallery = ({ query }) => {
-  const [page, setPage] = useState(1);
-  const [photos, setPhotos] = useState([]);
-  const curQuery = useRef("");
+export const ImageGallery = ({ photos }) => {
   const [selectedImage, setSelectedImage] = useState(null);
-  const [loading, setloading] = useState(false);
-  const [isError, setIsError] = useState(false);
-
-  useEffect(() => {
-    if (!query) return;
-    if (curQuery.current != query) setPhotos([]);
-    curQuery.current = query;
-
-    setloading(true);
-    const fetchPhotos = async () => {
-      try {
-        const photos = await getPhotos(query, page);
-        console.log(photos.results);
-        setPhotos((prevPhotos) => [...prevPhotos, ...photos.results]);
-      } catch (error) {
-        setIsError(true);
-        console.log(error.message);
-      } finally {
-        setloading(false);
-      }
-    };
-    fetchPhotos();
-  }, [page, query]);
-
-  const onLoadMore = () => {
-    setPage(page + 1);
-  };
 
   const openModal = (selectedImage) => {
     setSelectedImage(selectedImage);
@@ -51,7 +17,6 @@ export const ImageGallery = ({ query }) => {
 
   return (
     <div>
-      {isError && <ErrorMessage />}
       <ul className={style.gallery}>
         {photos.map(({ id, alt_description, urls, likes, user }) => (
           <li
@@ -63,10 +28,7 @@ export const ImageGallery = ({ query }) => {
           </li>
         ))}
       </ul>
-      {loading && <Loader />}
-      {photos.length > 0 && (
-        <LoadMoreBtn onClick={onLoadMore} setPage={setPage} />
-      )}
+
       {selectedImage && (
         <ImageModal
           isOpen={!!selectedImage}
